3effd8168ce1f5bec2aeb1e362c25cea480dcee1
Francisco Alarcón <f.alarcon.pontigo@gmail.com>
2017-03-21 01:43:34 -0300

    comenzando a llenar el arbol

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,10 @@ public class Main {
         System.out.println("Ingrese el numero del nodo");
         int valor = leer.nextInt();
         obj.setValor(valor);
+        obj.setleft(obj);
+        obj.left.setValor(4);
+        System.out.println(obj.left.getValor());
+        System.out.println(obj.getValor());
 
     }
 }
